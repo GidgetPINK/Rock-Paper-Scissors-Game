a3c41fcad656ca26277aa678733d3e9b6e324fed
Apryl Smith <gidgetpink@gmail.com>
2023-01-22 21:18:55 -0500

    added functions for userchoice and computer choice

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const computerChoice = document.getElementById('computer-choice');
-const userChoice = document.getElementById('user-choice');
-const result = document.getElementById('result');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}));
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+    if(randomNumber === 1){
+        computerChoice = 'rock'
+    }
+    if(randomNumber === 2){
+        computerChoice = 'scissors'
+    }
+    if(randomNumber === 3){
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
